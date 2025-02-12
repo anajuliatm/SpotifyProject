3549d1bd734eacaf6eb3f7345a9363cd42ac477c
Ana Julia Ticianeli <anajuliamarcelino02@gmail.com>
2025-02-12 19:20:50 -0300

    Arrumando os arquivos parar recomçar de onde eu parei

--- Aula2/Gabarito da Aula 2/src/App.jsx
+++ Aula2/Gabarito da Aula 2/src/App.jsx
@@ -1,26 +1,18 @@
-import React from "react";
-import Header from "./components/Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Artists from "./pages/Artists";
-import Artist from "./pages/Artist";
-import Songs from "./pages/Songs";
-import Song from "./pages/Song";
+//import { useState } from "react";
+//import viteLogo from "/vite.svg";
+//import "./App.css";
+
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/artists" element={<Artists />} />
-        <Route path="/artist/:id" element={<Artist />} />
-        <Route path="/songs" element={<Songs />} />
-        <Route path="/song/:id" element={<Song />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
+    <>
+    <Header />
+    <Main />;
+  </>
+  )
+}
 export default App;
--- Aula2/Gabarito da Aula 2/src/components/Header.jsx
+++ Aula2/Gabarito da Aula 2/src/components/Header.jsx
@@ -1,19 +1,12 @@
-import React from "react";
-import logoSpotify from "../assets/logo/spotify-logo.png";
-import { Link } from "react-router-dom";
-
+import React from 'react'
+import logoSpotify from '../assets/logo/spotify-logo.png'
 const Header = () => {
   return (
     <div className="header">
-      <Link to="/">
         <img src={logoSpotify} alt="Logo do Spotify" />
-      </Link>
-
-      <Link to="/" className="header__link">
-        <h1>Spotify</h1>
-      </Link>
+        <a className="header__link" href="/"> <h1>Spotify</h1> </a>
     </div>
   );
 };
 
-export default Header;
+export default Header
